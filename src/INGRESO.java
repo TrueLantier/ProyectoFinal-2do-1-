5af0c250c9ad1a123386e74b765c3758b3c25bc7
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-01-09 09:57:25 -0500

    Agregar datos sobre el programa y las fotos.

--- src/INGRESO.java
+++ src/INGRESO.java
@@ -1,2 +1,14 @@
+/**
+ * Aquí se programará la ventana de ingreso.
+ * Elementos que debe tener:
+ * Imágenes fitness y color.
+ * Bienvenida.
+ * Campo de texto donde el usuario debe escribir su nombre.
+ * Campo de texto para escribir clave de administrador o instructor.
+ * Botón que permita pasar a la siguiente pantalla.
+ * Al pie, algún tipo de mensaje.
+  */
+
 public class INGRESO {
+
 }
